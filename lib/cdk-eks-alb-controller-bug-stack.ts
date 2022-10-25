5c08d9737aff2bc3514d2c3991077d9040fad3b3
Sergey Zhukov <sergey@code-partners.com>
2022-10-26 01:32:01 +0400

    CDK stack reproducing the problem

--- lib/cdk-eks-alb-controller-bug-stack.ts
+++ lib/cdk-eks-alb-controller-bug-stack.ts
@@ -1,16 +1,56 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as eks from "aws-cdk-lib/aws-eks";
 
 export class CdkEksAlbControllerBugStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const cluster = new eks.Cluster(this, "eks-cluster", {
+      version: eks.KubernetesVersion.V1_21,
+      albController: {
+        version: eks.AlbControllerVersion.V2_4_1
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEksAlbControllerBugQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const ingress = {
+      apiVersion: "networking.k8s.io/v1",
+      kind: "Ingress",
+      metadata: {
+        name: "sample-ingress"
+      },
+      spec: {
+        ingressClassName: "alb",
+        defaultBackend: {
+          service: {
+            name: "default-http-backend",
+            port: {
+              number: 80
+            }
+          }
+        },
+        rules: [{
+          host: "example.com",
+          http: {
+            paths: [{
+              path: "/*",
+              pathType: "ImplementationSpecific",
+              backend: {
+                service: {
+                  name: "sample",
+                  port: {
+                    number: 80
+                  }
+                }
+              }
+            }]
+          }
+        }]
+      }
+    };
+
+    const manifest = new eks.KubernetesManifest(this, "ingress-manifest", {
+      cluster, manifest: [ingress],
+    });
   }
 }
